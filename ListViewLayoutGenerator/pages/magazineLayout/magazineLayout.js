80be4dffe3dc16abd0fd76f7e442a0c3a68bcf29
nanovazquez <mariano.vazquez@southworks.net>
2012-09-04 18:11:39 -0300

    added magazine sample page (initial version)

---
+++ ListViewLayoutGenerator/pages/magazineLayout/magazineLayout.js
@@ -0,0 +1,68 @@
+﻿/// <reference path="groupedItems.html" />
+(function () {
+    "use strict";
+
+    var appView = Windows.UI.ViewManagement.ApplicationView;
+    var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
+    var nav = WinJS.Navigation;
+    var ui = WinJS.UI;
+    var utils = WinJS.Utilities;
+
+    // the listView's settings
+    var gridLayoutOptions = {
+        groupHeaderPosition: 'left',
+        groupInfo: {
+            enableCellSpanning: true,
+            cellWidth: 1,
+            cellHeight: 1,
+        },
+    };
+
+    var magazineLayoutPageControl = ui.Pages.define("/pages/magazineLayout/magazineLayout.html", {
+
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+
+            // hide all appbarIncons, but home
+            document.querySelector('#appbar').winControl.showOnlyCommands(['cmdHome'], true);
+
+            var listView = element.querySelector('.groupeditemslist').winControl;
+            listView.groupHeaderTemplate = element.querySelector('.headerTemplate');
+            listView.itemTemplate = element.querySelector('.itemTemplate');
+            this.initializeLayout(listView, appView.value);
+            listView.element.focus();
+        },
+
+        // This function updates the ListView with new layouts
+        initializeLayout: function (listView, viewState) {
+            /// <param name="listView" value="WinJS.UI.ListView.prototype" />
+
+            // the listView does not support snapped mode
+            if (viewState != appViewState.snapped) {
+                listView.itemDataSource = Data.items.dataSource;
+                listView.groupDataSource = Data.groups.dataSource;
+                listView.layout = new ui.GridLayout(gridLayoutOptions);
+            }
+        },
+
+        // This function updates the page layout in response to viewState changes.
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+            /// <param name="viewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
+            /// <param name="lastViewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
+
+            var listView = document.querySelector('.groupeditemslist').winControl;
+            if (lastViewState !== viewState) {
+                if (lastViewState === appViewState.snapped || viewState === appViewState.snapped) {
+                    var handler = function (e) {
+                        listView.removeEventListener('contentanimating', handler, false);
+                        e.preventDefault();
+                    }
+                    listView.addEventListener('contentanimating', handler, false);
+                    this.initializeLayout(listView, viewState);
+                }
+            }
+        }
+    });
+})();
